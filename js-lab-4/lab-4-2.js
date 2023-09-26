52195336dbab49a5f9bab3036e4e6b4aaecc76db
bingjetli <bingjetli88@gmail.com>
2023-09-26 16:16:57 -0400

    added html-lab-4

--- js-lab-4/lab-4-2.js
+++ js-lab-4/lab-4-2.js
@@ -4,11 +4,22 @@
 
 //################## CREATE YOUR AVERAGE FUNCTION
 //This function takes five numbers and returns their average to one decimal place.
-const averageOf = (n1, n2, n3, n4, n5) => (n1 + n2 + n3 + n4 + n5) / 5;
+const averageOf = (n1, n2, n3, n4, n5) => {
+    return ((n1 + n2 + n3 + n4 + n5) / 5).toFixed(1);
+};
 
 
 //################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
 //Used `.toFixed()` from MDN : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 console.log('The average of [5, 10, 15, 20, 25] is ' + averageOf(
     5, 10, 15, 20, 25
-).toFixed(1));
+));
+
+const n1 = 66;
+const n2 = 78;
+const n3 = 99;
+const n4 = 97;
+const n5 = 86;
+
+if(averageOf(n1, n2, n3, n4, n5) >= 70) alert('Success');
+else alert('Review required');
